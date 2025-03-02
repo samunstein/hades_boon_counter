689f60acf7cba1c21f37b4323c428a965f90efef
samunstein <samu.p.jussila@gmail.com>
2025-03-02 12:14:47 +0200

    Read god primarily from icon text instead of god attribute
    There is at least one boon that is actually from Demeter, but the god field says Zeus

--- hadesboons.py
+++ hadesboons.py
@@ -108,9 +108,7 @@ def read_traits(data, lang):
     for trait in traitdict.values():
         trait_d = trait[1]
         name = lang[trait_d["Name"]] if trait_d["Name"] in lang else trait_d["Name"]
-        if "God" in trait_d:
-            trait_list.append(Trait([trait_d["God"]], name, trait_d["Rarity"] if "Rarity" in trait_d else "Common"))
-        elif LIST_HAMMER and "Frame" in trait_d and trait_d["Frame"] == "Hammer":
+        if LIST_HAMMER and "Frame" in trait_d and trait_d["Frame"] == "Hammer":
             trait_list.append(Trait(["Hammer"], name, "Common"))
         elif "Frame" in trait_d and trait_d["Frame"] == "Duo":
             gods_re = re.match("([A-Za-z]+)_([A-Za-z]+)_[0-9]+", trait_d["Icon"])
@@ -118,11 +116,12 @@ def read_traits(data, lang):
             trait_list.append(Trait(list(gods), name, "Duo"))
         elif "Icon" in trait_d and "Chaos_Blessing" in trait_d["Icon"]:
             trait_list.append(Trait(["Chaos"], name, trait_d["Rarity"]))
-        elif "Icon" in trait_d and "Rarity" in trait_d:
+        elif "Icon" in trait_d and "Rarity" in trait_d and re.match("Boon_([A-Za-z]+)_[0-9]+", trait_d["Icon"]):
             god_re = re.match("Boon_([A-Za-z]+)_[0-9]+", trait_d["Icon"])
-            if god_re is not None:
-                god = god_re.groups()
-                trait_list.append(Trait(list(god), name, trait_d["Rarity"]))
+            god = god_re.groups()
+            trait_list.append(Trait(list(god), name, trait_d["Rarity"]))
+        elif "God" in trait_d:
+            trait_list.append(Trait([trait_d["God"]], name, trait_d["Rarity"] if "Rarity" in trait_d else "Common"))
     return trait_list
 
 
